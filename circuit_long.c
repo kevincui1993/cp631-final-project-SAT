1dad3eb9ec4269ad1b6b2fad68a878268a8cdb12
Kevin Cui <kevin.cui@veeva.com>
2020-12-07 20:29:12 -0500

    serial SAT solution

---
+++ circuit_long.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <math.h>
+#include <time.h> 
+
+#define INPUTS 18
+
+// function to convert decimal to binary 
+void decToBinary(int num, int binValue[]) 
+{
+    for (int j = 0; j < INPUTS; j++)
+    {
+        if ( num > 0 )
+        {
+            // storing remainder in binary array 
+            binValue[j] = num % 2; 
+            num = num / 2;
+        }
+        else
+        {
+            binValue[j] = 0;
+        }
+    }
+}
+
+int validateCircuit(int binValue[])
+{
+    int result = 0;
+
+    int A = binValue[0];
+    int B = binValue[1];
+    int C = binValue[2];
+    int D = binValue[3];
+    int E = binValue[4];
+    int F = binValue[5];
+    int G = binValue[6];
+    int H = binValue[7];
+    int I = binValue[8];
+    int J = binValue[9];
+    int K = binValue[10];
+    int L = binValue[11];
+    int M = binValue[12];
+    int N = binValue[13];
+    int O = binValue[14];
+    int P = binValue[15];
+    int Q = binValue[16];
+    int R = binValue[17];
+
+    result = (P && (Q && R) && ((G && H) && !I) && ((((A || B) && !C) || ((B && C) || ((D || E) && F))) && ((((L || M) && N) || (J && K)) && !O)));
+
+    return result;
+}
+
+int main(int argc, char *argv[])
+{
+    //each input has binary value, there are N inputs
+    int combinations = pow(2, INPUTS) - 1;
+    int results[combinations];
+    
+    int binValue[INPUTS];
+
+    //open file output.txt in write mode 
+    FILE *fptr = fopen("circuitoutput.txt", "w"); 
+    if (fptr == NULL) 
+    { 
+        printf("Could not open file"); 
+        return 0; 
+    }
+
+    clock_t begin = clock();
+    for (int i = 0; i <= combinations; i++)
+    {
+        
+        decToBinary(i,binValue);
+        results[i] = validateCircuit(binValue);
+        if (results[i] == 1)
+        {
+            for (int j = 0; j < INPUTS; j++)
+                fprintf(fptr, "%d", binValue[j]);
+            fprintf(fptr,"\n");
+        }
+    }
+    clock_t end = clock();
+    // calculate elapsed time by finding difference (end - begin)
+    printf("Time elpased is %.4f seconds", (double)(end - begin) / CLOCKS_PER_SEC);
+
+    return 0;
+}
